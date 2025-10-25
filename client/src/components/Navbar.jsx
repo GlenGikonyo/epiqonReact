3e00f9933be00a9b4149d7bb03310d3ae6aa9909
GlenGikonyo <gikonyoglen@gmail.com>
2025-10-25 20:38:32 +0300

    refactor: improve Navbar structure and update Portfolio page title

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../assets/images/ei_1644409695507-removebg-preview.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/ei_1644409695507-removebg-preview.png";
 
 const Navbar = () => {
   return (
@@ -17,19 +17,39 @@ const Navbar = () => {
             <div className="collapse navbar-collapse main-menu">
               <div className="nav-menu-wrapper">
                 <ul className="navbar-nav mr-auto" id="menu">
-                  <li><Link className="nav-link" to="/">home</Link></li>
-                  <li className="nav-item"><Link className="nav-link" to="/about">about us</Link></li>
-                  <li className="nav-item"><Link className="nav-link" to="/services">services</Link></li>
-                  <li className="nav-item"><Link className="nav-link" to="/portfolio">portfolio</Link></li>
+                  <li>
+                    <Link className="nav-link" to="/">
+                      home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/about">
+                      about us
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/services">
+                      services
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/portfolio">
+                      training packages
+                    </Link>
+                  </li>
                   <li className="nav-item highlighted-menu">
-                    <Link className="nav-link" to="/contact-us">Contact Us</Link>
+                    <Link className="nav-link" to="/contact-us">
+                      Contact Us
+                    </Link>
                   </li>
                 </ul>
               </div>
 
               {/* Let’s Start Button Start */}
               <div className="header-btn d-inline-flex">
-                <Link to="/contact-us" className="btn-default">Contact Us</Link>
+                <Link to="/contact-us" className="btn-default">
+                  Contact Us
+                </Link>
               </div>
               {/* Let’s Start Button End */}
             </div>
--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -3,13 +3,15 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import PageHeader from "../components/PageHeader";
 // import Projects from "../components/Projects";
+// import OurServices from "../components/HeroServices";
 
 const Portfolio = () => {
   return (
     <>
       <Navbar />
-      <PageHeader title={"Projects"} />
+      <PageHeader title={"Training Packages"} />
       {/* <Projects /> */}
+      {/* <OurServices /> */}
       <Footer />
     </>
   );
